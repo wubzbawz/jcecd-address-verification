065f2cd1249d41602d950f2bff2731da17a7a832
wubzbawz <p0941814@gmail.com>
2026-09-09 20:10:40 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='jcecd-address-lookup-v3-1-split';
-const ASSETS=['./','./index.html','./data-01.tsv','./data-02.tsv','./data-03.tsv','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{ if(e.request.method!=='GET') return; e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
